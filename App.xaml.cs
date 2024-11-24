3677ca58b0cf401d3c0ada0e6145fcc9f9a096d0
IONELA-ANAMARIA TOMUȚĂ <ionela.tomuta@stud.ubbcluj.ro>
2024-11-24 22:29:05 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TomutaIonelaLab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TomutaIonelaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
