47045b2bf598b6dccead4641c9ecd7289b6983b2
IONELA-ANAMARIA TOMUȚĂ <ionela.tomuta@stud.ubbcluj.ro>
2024-12-01 23:33:41 +0200

    Laborator7

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace TomutaIonelaLab7
+﻿using System;
+using TomutaIonelaLab7.Data;
+using System.IO;
+
+namespace TomutaIonelaLab7
+
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
